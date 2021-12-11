7761ab1d00622b4d461c3bdd95b9fd64e8532b28
zeeshan-wpbrigade <zeeshan.wpbrigade@gmail.com>
2021-12-11 17:01:22 +0500

    Update Style

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,45 +1,42 @@
 $(document).ready(function () {
-    $(".menu-btn").click(function () {
-      $(this).toggleClass("close");
-    });
-
-    $(".menu-btn").click(function () {
-        $(".side-menu").toggleClass("open");
-        $(".container").toggleClass("content-move");
-      });
-
-
-      $(".item-1").hover(function() {
-        $('body').css('background-color' , 'grey');
-      }, function() {
-        $('.flyout').addClass('hidden');
-      });
-      
-
-
-    
-    //   slider 
-      $('.owl-carousel').owlCarousel({
-        loop:true,
-        responsiveClass:true,
-        dots:false,
-        responsive:{
-            0:{
-                items:1,
-                nav:false
-            },
-            600:{
-                items:4,
-                nav:false
-            },
-            1000:{
-                items:4,
-                nav:false,
-                loop:false
-            }
-        }
-    });
-
-
-
+  // 1. Make the Humberger icon to cross
+  $(".menu-btn").click(function () {
+    $(this).toggleClass("close");
   });
+
+  // 2. Move container as just like Side bar moves out
+  // 3. Open Side bar with add open class
+  $(".menu-btn").click(function () {
+    $(".side-menu").toggleClass("open");
+    $("body").toggleClass("overflow");
+    $(".container").toggleClass("content-move");
+  });
+
+  // 4.slider
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    responsiveClass: true,
+    dots: false,
+    navText: [
+      "<img src='assets/img/left.png'>",
+      "<img src='assets/img/right.png'>",
+    ],
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+      },
+      600: {
+        items: 4,
+        nav: true,
+      },
+      1000: {
+        items: 4,
+        nav: true,
+        loop: true,
+      },
+    },
+  });
+
+  // Main Document ends
+});
