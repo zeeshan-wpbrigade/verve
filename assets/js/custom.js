905873c347bd1254f6fd4a5350d96138cfb17faa
zeeshan-wpbrigade <zeeshan.wpbrigade@gmail.com>
2021-12-14 17:00:34 +0500

    Update Style

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -8,11 +8,22 @@ $(document).ready(function () {
   // 3. Open Side bar with add open class
   $(".menu-btn").click(function () {
     $(".side-menu").toggleClass("open");
-    $("body").toggleClass("overflow");
     $(".container").toggleClass("content-move");
   });
 
-  // 4. Full Screen Mode
+  // 4. Open info tab
+  $(".info-btn").click(function () {
+    $(".information").toggleClass("open-2");
+    $(".container").toggleClass("content-move-2");
+  });
+ 
+  // 5. Close info tab by Cross
+  $(".close-info").click(function () {
+    $(".information").toggleClass("open-2");
+    $(".container").toggleClass("content-move-2");
+  });
+
+  // 6. Full Screen Mode
   $("#full-screen-btn").on("click", function () {
     if (!document.fullscreenElement) {
       let e = document.querySelector("html");
@@ -22,7 +33,7 @@ $(document).ready(function () {
     }
   });
 
-  // 5. owl slider
+  // 6. owl slider
   $(document).ready(function () {
     if ($(window).width() > 767) {
       startCarousel();
@@ -42,14 +53,8 @@ $(document).ready(function () {
     $(".owl-carousel").owlCarousel({
       navigation: true, // Show next and prev buttons
       slideSpeed: 500,
-      margin: 10,
-      paginationSpeed: 400,
-      autoplay: true,
+      dots: false,
       items: 4,
-      itemsDesktop: false,
-      itemsDesktopSmall: false,
-      itemsTablet: false,
-      itemsMobile: false,
       loop: true,
       nav: true,
       navText: [
@@ -64,11 +69,40 @@ $(document).ready(function () {
     owl.addClass("off");
   }
 
+  // 7. Hide Information ection on click out side the information conatiner
+  $(".container").click(function (e) {
+    if (e.target.className !== "information") {
+      $(".information").removeClass("open-2");
+      $(".container").removeClass("content-move-2");
+    }
+  });
+
+  // 7. Hide Side Menu Section on click out side the Side Menu conatiner
+  $(".container").click(function (e) {
+    if (e.target.className !== "side-menu") {
+      $(".side-menu").removeClass("open");
+      $(".container").removeClass("content-move");
+      // Condtion if Menu icon in Homberger then dont add class
+      if ($(".menu-btn").hasClass("close")) {
+        $(".menu-btn").removeClass("close");
+      }
+    }
+  });
+  // Video Play and Pause on clcik the video section for Mobile Version
+  $(document).ready(function () {
+    if ($(window).width() < 767) {
+      $("video").click(function () {
+        this.paused ? this.play() : this.pause();
+      });
+    }
+  });
+
   // Main Document ends
 });
 
 // JS Out from the jQuery Tag
-// 6. on Hover change bg of body
+
+// 8. on Hover change bg of body
 function changebg(src, type) {
   if (type === "image") {
     $(".image").css("display", "block");
@@ -82,3 +116,33 @@ function changebg(src, type) {
     document.getElementById("playrest").play();
   }
 }
+
+// 9. Second Slider Custom slider wuth JS
+
+var currentSlide = 0;
+const slides = document.querySelectorAll(".slide");
+
+const init = (n) => {
+  slides.forEach((slide) => {
+    slide.style.display = "none";
+  });
+  slides[n].style.display = "block";
+};
+document.addEventListener("DOMContentLoaded", init(currentSlide));
+
+const right = () => {
+  currentSlide >= slides.length - 1 ? (currentSlide = 0) : currentSlide++;
+  init(currentSlide);
+};
+
+const left = () => {
+  currentSlide <= 0 ? (currentSlide = slides.length - 1) : currentSlide--;
+  init(currentSlide);
+};
+
+document.querySelector(".right").addEventListener("click", right);
+document.querySelector(".left").addEventListener("click", left);
+
+setInterval(() => {
+  right();
+}, 5000);
